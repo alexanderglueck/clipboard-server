4f939db2b6bafde668430c035a318138487e77ec
Alexander Glück <github@alexanderglueck.at>
2022-06-08 21:42:39 +0200

    Add missing device_id column to posts table

--- database/migrations/2021_01_13_184247_create_posts_table.php
+++ database/migrations/2021_01_13_184247_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('device_id')->constrained();
             $table->longText('content');
             $table->timestamps();
         });
